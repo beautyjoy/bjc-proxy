a11495fd7d498e4a7c7ee41d385ca4f36111861c
Michael Ball <cycomachead@gmail.com>
2015-04-04 02:04:33 -0700

    Add keep alive to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Heroku defines the environment variable PORT, and requires the binding address to be 0.0.0.0
-var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var host = process.env.HOST ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 8080;
 
 var cors_proxy = require('./lib/cors-anywhere');
@@ -21,3 +21,8 @@ cors_proxy.createServer({
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+// Keep Heroku From Idling, every 5 mins.
+var keepAlive = require('keep-alive')
+
+keepAlive.add(process.env.HOST, 5*1000*60);
